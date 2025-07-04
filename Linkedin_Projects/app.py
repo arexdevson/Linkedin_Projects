5723dbda0cd5f49f1b831a4acbb8d0c092b05a45
LextheLuthor <68922690+arexdevson@users.noreply.github.com>
2025-07-04 08:55:20 -0300

    versão cloud agora vai

--- Linkedin_Projects/app.py
+++ Linkedin_Projects/app.py
@@ -17,6 +17,7 @@ nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 nltk.download("punkt", download_dir=nltk_data_path)
 nltk.download("words", download_dir=nltk_data_path)
 nltk.data.path.append(nltk_data_path)
+nltk.download('punkt_tab')
 
 common_words = set(words.words())
 
